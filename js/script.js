8fc56169da0a8c501b46f21bf7f5f1f7b81bb1c9
Johnny Edick <johnny@atomicinfotech.com>
2015-11-24 18:00:06 -0800

    anther test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 SC.initialize({
-    client_id: 'b301c8470ec1bde5474c91bfda3dc58b',
-    redirect_uri: 'http://thebushybrow.github.io/soundstats/callback.html'
+    client_id: "b301c8470ec1bde5474c91bfda3dc58b",
+    redirect_uri: "http://johnnyedick.com/callback",
 });
 
-$(document).ready(function() {
-  SC.get('/tracks', { genres: 'rap' }, function(tracks) {
-    $(tracks).each(function(index, track) {
-      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-    });
-  });
+/**
+Once that's done you are all set and ready to call the SoundCloud API. 
+**/
+
+
+
+/**
+Call to the SoundCloud API. 
+Retrieves list of tracks, and displays a list with links to the tracks showing 'tracktitle' and 'track duration'
+**/
+
+var userId = 39090345; // user_id of Prutsonic
+
+SC.get("/tracks", {
+    user_id: userId,
+    limit: 100
+}, function (tracks) {
+    var tmp = '';
+
+    for (var i = 0; i < tracks.length; i++) {
+        tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
+
+        $("<li/>").html(tmp).appendTo("#track-list");
+    }
 });
