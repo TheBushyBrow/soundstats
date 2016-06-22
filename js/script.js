1df9daf6fd68d3a652fceb4ca46bcc0327eaec99
Johnny Edick <johnny@atomicinfotech.com>
2016-06-22 13:35:53 -0700

    update to pull tracks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 SC.initialize({
     client_id: "b301c8470ec1bde5474c91bfda3dc58b",
-    redirect_uri: "http://johnnyedick.com/callback",
+    redirect_uri: "http://johnnyedick.com/callback.html",
 });
 
 /**
@@ -14,17 +14,16 @@ Call to the SoundCloud API.
 Retrieves list of tracks, and displays a list with links to the tracks showing 'tracktitle' and 'track duration'
 **/
 
-var userId = 39090345; // user_id of Prutsonic
+  var userId = 39090345; // user_id of Prutsonic
 
-SC.get("/tracks", {
-    user_id: userId,
-    limit: 100
-}, function (tracks) {
-    var tmp = '';
+  SC.get("/tracks", {
+     user_id: userId,
+     limit: 100
+ }).then(function (tracks) {
+     var tmp = '';
 
-    for (var i = 0; i < tracks.length; i++) {
-        tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
-
-        $("<li/>").html(tmp).appendTo("#track-list");
-    }
-});
+     for (var i = 0; i < tracks.length; i++) {
+         tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
+         $("<li/>").html(tmp).appendTo("#track-list");
+     }
+ });
